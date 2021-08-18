c69983668f5730683001cb46b3f2063c0c1e4670
danomwa <danomwaopiyo@gmail.com>
2021-08-18 10:28:56 +0300

    main.h file

---
+++ main.h
@@ -0,0 +1,16 @@
+#ifndef HOLBERTON_H
+#define HOLBERTON_H
+
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <string.h>
+#include <errno.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <signal.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <dirent.h>
+
+ssize_t _prompt(char **argv, int *flag, char **env);
