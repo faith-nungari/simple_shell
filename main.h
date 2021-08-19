5a0b35b31cd792e57c43d507fd4806127cbaba15
danomwa <danomwaopiyo@gmail.com>
2021-08-19 09:28:38 +0300

    10- not allowed to use strtok

--- main.h
+++ main.h
@@ -16,5 +16,6 @@
 ssize_t _prompt(char **argv, int *flag, char **env);
 int _exitt(char *av, int num);
 int _print_env(char *av, char **env);
+void _path(char **av, char *env, char *av_0, int av_l, int *p_find);
 
 #endif
