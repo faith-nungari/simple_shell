a7470cb2eae7cefc6feac5a12d3e0c090821f6c5
Faith <faykonye2@gmail.com>
2021-08-19 20:07:57 -0700

    A description of this repository

---
+++ main.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * main - creates a simple shell
+ * @ac: argument count
+ * @av: argument vectors
+ * @env: environmental variables
+ * Return: 0 on success
+ */
+int main(int ac, char **av, char **env)
+{
+	(void)ac;
+	(void)av;
+
+	prompt(env);
+
+	return (0);
+}
