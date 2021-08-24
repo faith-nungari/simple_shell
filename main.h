2345093098102b93868520e0f77363b6be330a1d
DAN OMWA OPIYO <danomwaopiyo@gmail.com>
2021-08-24 09:06:16 +0300

    main - master

--- main.h
+++ main.h
@@ -17,5 +17,6 @@ ssize_t _prompt(char **argv, int *flag, char **env);
 int _exitt(char *av, int num);
 int _print_env(char *av, char **env);
 void _path(char **av, char *env, char *av_0, int av_l, int *p_find);
-
+void fill(char *str, char *p);
+char **_strtok(char *str);
 #endif
